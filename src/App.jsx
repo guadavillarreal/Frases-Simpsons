1b8800804bc0968650f68b3bbaa8b17d926219de
guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-04-22 00:39:37 -0300

    agrege comentarios de guias, falta realizar el msj de error para el us xsi no carga la app

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,52 @@ import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [frasePersonaje, setFrasePersonaje] = useState({})
-
-  useEffect(()=>{
+  //p guardar lo que devuelve la api-statr
+  const [frasePersonaje, setFrasePersonaje] = useState({});
+  //ciclo de vida
+  useEffect(() => {
+    //llamada a funcion
     consultarAPI();
   }, []);
+  //fuera de la api para poder usarla afuera de la api
+  const consultarAPI = async () => {
+    //cuando se utiliza solicitudes fech se suele usar try-catch, 
+    //try si la api func ok , catch xsi el codigo-api(error mio) falla se ejecuta 
+    try {
+      //app fech api:viaja al serv xlo q nose el tiempo de respuesta x la promesa de respuesta
+      //async-await: quiere decir que hay que esperar la resp antes de seguir
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      //json-metd de js, del obj respond, puede tardar xloq ponemos el metd await
+      //para extraer el body de la resp
+      const datos = await respuesta.json();
+      console.log(respuesta);
+      console.log(datos);
+      //llamada a la func
+      //guardar los datos del api en el state
+      setFrasePersonaje(datos[0]);
+    } catch (error) {
+      console.log(error);
+      // agregar un mensaje para el usuario xel error
+    //catch xsi el codigo-api(error mio) falla se ejecuta si es mi error lo arreglo sino veo la api , puede estar fallando su serv
+    }
+  };
 
-  const consultarAPI = async()=>{
-      try{
-        const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-        const datos = await respuesta.json()
-        console.log(respuesta);
-        console.log(datos);
-        //guardar los datos del api en el state
-        setFrasePersonaje(datos[0])
-      }catch(error){
-        console.log(error);
-        // agregar un mensaje para el usuario
-      } 
-
- 
-  }
-  
   return (
     <>
-       <Container className="text-center my-5">
+      <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-       <Frase frasePersonajeProps={frasePersonaje}></Frase>
-        <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
+        {/*envio de datos-props desde setFrasePersonaje*/}
+        <Frase frasePersonajeProps={frasePersonaje}></Frase>
+        {/*cuando hacen click en el btn invoca la func que llama ala api, por eso lo realizamos afuera de la app- es una func sin param xlo que no se declara
+        como func de flecha, si le llegase a porne los () recargaria antes de tiempo */}
+        <Button variant="warning" onClick={consultarAPI}>
+          Obtener frase
+        </Button>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,18 +1,24 @@
 import { Card } from "react-bootstrap";
-
+//obj props frasePersonajeProps-desestructurado, donde estan las prop
 const Frase = ({frasePersonajeProps}) => {
   return (
     <section className="my-5">
+      {/*nombre del personajeindica de donde viene la info-api */}
       <h2>{frasePersonajeProps.character}</h2>
       <hr />
+      {/* */}
+      {/*frasePersonajeProps.image: direccion de la img de laapi*/}
+      {/*alt:nombre del personajeindica de donde viene la info-api */}
       <img className="mb-3" src={frasePersonajeProps.image} alt={frasePersonajeProps.character} />
       <Card>
         <Card.Body>
           <figure>
             <blockquote className="blockquote">
+            {/*frasePersonajeProps.quote: la frace que viene de la api*/}
               <p>{frasePersonajeProps.quote}</p>
             </blockquote>
             <figcaption className="blockquote-footer">
+      {/*nombre del personajeindica de donde viene la info-api */}
             {frasePersonajeProps.character} in <cite title="Source Title">The Simpsons</cite>
             </figcaption>
           </figure>
